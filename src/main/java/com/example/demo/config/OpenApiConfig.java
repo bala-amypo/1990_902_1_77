185fae442d47abcca9fc84e7a99d945a7a9cb464
bala-amypo <bala@amypo.in>
2025-12-21 13:58:34 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +13,12 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Student Skill Gap Analyzer API")
+                        .version("1.0")
+                        .description("API for analyzing student skill gaps and generating recommendations"))
                 .servers(List.of(
-                        new Server().url("https://9005.vs.amypo.ai")
+                        new Server().url("https://9556.pro604cr.amypo.ai/")
                 ));
-        }
+    }
 }
